940cad9a5b3cc9b60b6420e343fa02fedd2ce17e
viek4nag <vivek4nag@gmail.com>
2024-09-18 01:25:48 +0530

    choices added

--- script.js
+++ script.js
@@ -10,3 +10,43 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
 const scissor_div = document.getElementById("s")
+
+function gtetComputerChoice (){
+    const choices = ['r', 'p', 's']; // storing rock pape scissor for computer to select in array
+    // console.log(Math.floor(Math.random() * 3));
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]; // getting random computer choice
+    
+}
+// console.log(gtetComputerChoice());
+
+
+function game(userChoice){
+    const computerChoice = gtetComputerChoice();
+    console.log("User choice is =>" + userChoice);
+    console.log("comp choice is =>" + computerChoice);
+    
+    
+}
+
+function main() {
+    rock_div.addEventListener("click", function(){
+        game("r")
+        
+    })
+    paper_div.addEventListener("click", function(){
+        // console.log("you clicked on paper");
+        game("p")
+
+        
+    })
+    scissor_div.addEventListener("click", function(){
+        // console.log("you clicked on scissor");
+        game("s")
+
+        
+    })
+}
+
+main();
+
